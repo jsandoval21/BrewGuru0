46b90b1563d121ec00e94ffc4cb4f68441376f78
jsandoval21 <juansndvl89@gmail.com>
2018-11-05 18:48:32 -0800

    Initial project version

---
+++ app/src/main/java/com/fatmenbrews/brewguru/BrewActivity.java
@@ -0,0 +1,13 @@
+package com.fatmenbrews.brewguru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brew);
+    }
+}
